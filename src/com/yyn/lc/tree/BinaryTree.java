ef0ecb9e8125e85f418c6748dbb0f8b1a4b71f72
Jimi Yu <Jimi Yu>
2016-05-25 20:26:01 -0400

    resolved minimum depth of binary tree

--- src/com/yyn/lc/tree/BinaryTree.java
+++ src/com/yyn/lc/tree/BinaryTree.java
@@ -6,6 +6,10 @@ package com.yyn.lc.tree;
  * Given a binary tree, find its maximum depth.
  * The maximum depth is the number of nodes along the longest path from
  * the root node down to the farthest leaf node.
+ *
+ * 2. Minimum Depth of Binary Tree
+ * Given a binary tree, find its minimum depth.
+ * The minimum depth is the number of nodes along the shortest path from the root node down to the nearest leaf node.
  */
 public class BinaryTree {
     /**
@@ -40,24 +44,55 @@ public class BinaryTree {
 
         int depth = 0;
 
-        depth = travel(root, 1, depth);
+        depth = travelMax(root, 1, depth);
 
         return depth;
     }
 
-    private int travel(TreeNode node, int level, int depth) {
+    private int travelMax(TreeNode node, int level, int depth) {
         if (node.left == null && node.right == null) {
             depth = Math.max(depth, level);
         }
 
         if (node.left != null) {
-            depth = travel(node.left, level + 1, depth);
+            depth = travelMax(node.left, level + 1, depth);
         }
 
         if (node.right != null) {
-            depth = travel(node.right, level + 1, depth);
+            depth = travelMax(node.right, level + 1, depth);
         }
 
         return depth;
+    }
+
+    /**
+     * @param root: The root of binary tree.
+     * @return: An integer.
+     */
+    public int minDepth(TreeNode root) {
+        if(root == null){
+            return 0;
+        }
+
+        int depth = Integer.MAX_VALUE;
+        depth = travelMin(root, depth, 1);
+
+        return depth;
+    }
+
+    private int travelMin(TreeNode node, int depth, int level){
+        if(node.left == null && node.right == null){
+            depth = Math.min(depth, level);
+        }
+
+        if(node.left != null){
+            depth = travelMin(node.left, depth, level+1);
+        }
+
+        if(node.right != null){
+            depth = travelMin(node.right, depth, level+1);
+        }
+
+        return  depth;
     }
 }
